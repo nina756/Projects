3064ff9a128f757d7e9fcd43074a419694dba996
nina756 <88156545+nina756@users.noreply.github.com>
2024-06-04 13:58:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.getElementById('jpeg-image').src = 'https://nina756.github.io/Projects/20230906_170923.jpg';
+// document.getElementById('jpeg-image').src = 'https://nina756.github.io/Projects/20230906_170923.jpg';
