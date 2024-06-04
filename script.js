3b78124fcfc1a9ff7ca0d32b42afc3314a222db5
nina756 <88156545+nina756@users.noreply.github.com>
2024-06-04 12:57:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.getElementById('jpeg-image').src = 'https://nina756.github.io/Projects/cat.jpg';
+document.getElementById('jpeg-image').src = 'https://nina756.github.io/Projects/20230906_170923.jpg';
